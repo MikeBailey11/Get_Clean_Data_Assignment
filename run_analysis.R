7a7fe83d53dd9944532fb30bd36292bb35e2a904
Michael Bailey <nea_and_mike@bigpond.com>
2016-02-29 14:40:30 +1100

    Assignment
    Assignment upload

---
+++ run_analysis.R
@@ -0,0 +1,47 @@
+# Read and convert the data
+features <- read.csv('./UCI HAR Dataset/features.txt', header = FALSE, sep = ' ')
+features <- as.character(features[,2])
+
+data.train.x <- read.table('./UCI HAR Dataset/train/X_train.txt')
+data.train.activity <- read.csv('./UCI HAR Dataset/train/y_train.txt', header = FALSE, sep = ' ')
+data.train.subject <- read.csv('./UCI HAR Dataset/train/subject_train.txt',header = FALSE, sep = ' ')
+
+data.train <-  data.frame(data.train.subject, data.train.activity, data.train.x)
+names(data.train) <- c(c('subject', 'activity'), features)
+
+data.test.x <- read.table('./UCI HAR Dataset/test/X_test.txt')
+data.test.activity <- read.csv('./UCI HAR Dataset/test/y_test.txt', header = FALSE, sep = ' ')
+data.test.subject <- read.csv('./UCI HAR Dataset/test/subject_test.txt', header = FALSE, sep = ' ')
+# Combine X_train, y_train, subject_train, X_test, y_test and subject_test
+# into a new data frame data.test, and rename the colname of new data frame.
+data.test <-  data.frame(data.test.subject, data.test.activity, data.test.x)
+names(data.test) <- c(c('subject', 'activity'), features)
+
+# Merge the training and the test sets to create one data set
+data.all <- rbind(data.train, data.test)
+
+# Extract only the mean or standard deviation for each measurement
+col.select <- grep('mean|std', features)
+data.sub <- data.all[,c(1,2,col.select + 2)]
+
+# Use descriptive activity names for the activities in the data set
+activity.labels <- read.table('./UCI HAR Dataset/activity_labels.txt', header = FALSE)
+activity.labels <- as.character(activity.labels[,2])
+data.sub$activity <- activity.labels[data.sub$activity]
+
+# Label the data set with descriptive variable names
+name.new <- names(data.sub)
+name.new <- gsub("[(][)]", "", name.new)
+name.new <- gsub("^t", "TimeDomain_", name.new)
+name.new <- gsub("^f", "FrequencyDomain_", name.new)
+name.new <- gsub("Acc", "Accelerometer", name.new)
+name.new <- gsub("Gyro", "Gyroscope", name.new)
+name.new <- gsub("Mag", "Magnitude", name.new)
+name.new <- gsub("-mean-", "_Mean_", name.new)
+name.new <- gsub("-std-", "_StandardDeviation_", name.new)
+name.new <- gsub("-", "_", name.new)
+names(data.sub) <- name.new
+
+# Creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+data.tidy <- aggregate(data.sub[,3:81], by = list(activity = data.sub$activity, subject = data.sub$subject),FUN = mean)
+write.table(x = data.tidy, file = "data_tidy.txt", row.names = FALSE)
